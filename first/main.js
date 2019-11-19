0f97ec803f86da1cdf98c482591841932362e97d
sinarah22 <sinarahmannejad@gmail.com>
2019-11-19 11:19:20 -0800

    lots of things

--- first/main.js
+++ first/main.js
@@ -47,3 +47,56 @@ console.log(doubleTheVal(12));
 
 let sayHi = () => "hi" ;
 console.log(sayHi());
+
+
+//********************************************** */
+
+
+
+
+
+let john={
+    name: "john doe",
+    hobbies:["dancing","playing","running"],
+    activity : function(){
+        console.log(this.hobbies);
+    }
+};
+john.activity();
+
+//self ivoking functions
+//by putting one more () before and after the function it will automatically call itself
+let satHello = (()=>{
+    console.log("HELL OOOOOOO!");
+})();
+
+//events to web page
+//document.addEventListener("whats the actions?","whats the function");
+document.getElementById("test").addEventListener("click",annoyingAlert);
+function annoyingAlert (){
+    alert("I am an annoying Alert");
+}
+
+
+//*************************** */
+//map, filter, foreach ...
+let primeNumbers = [2,3,5,7,11,13,17,19];
+console.log(primeNumbers);
+//we made the function below shorter
+let doubleTheValues =  primeNumbers.map(number => number*2);
+console.log("Double the values",doubleTheValues);
+
+let lessThan10 = primeNumbers.filter(function (number){
+    return number< 10 ;
+});
+console.log('less than ten',lessThan10);
+
+
+/*let multiply = (firstValue) =>{
+    return function(secondValue){
+        return firstValue*secondValue;
+    }
+}*/
+//shorter version of the function on top
+let multiply = firstValue => (secondValue => firstValue*secondValue);
+console.log(multiply(4)(5));
